16371aca999660f7b98456a811d5951b0683f9f0
Phanikumar Palaparthi <ppk.phanikumar@gmail.com>
2018-04-01 15:06:13 +0530

    sync-crawler complete

--- LinkCrawler/linkcrawler/downloader.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-class Downloader(object):
-    def __init__(self, **kwargs):
-        return super().__init__(**kwargs)
-
-    def getContent(self, link):
-        return requests.get(link, verify=True)
